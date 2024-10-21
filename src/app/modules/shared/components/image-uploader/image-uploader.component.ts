7c119ab185c106d79e4b937e190514e2b44e84e4
Andre <andrekok@discovery.bank>
2024-10-21 14:54:22 +0200

    Compress images for uploading

--- src/app/modules/shared/components/image-uploader/image-uploader.component.ts
+++ src/app/modules/shared/components/image-uploader/image-uploader.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { NgxImageCompressService } from 'ngx-image-compress';
 import { ModalTypes } from 'src/app/enums/app.enums';
 import { UploadImageRequest } from 'src/app/models/upload-image-request.model';
 import { AppModalService } from 'src/app/services/app-modal/app-modal.service';
@@ -13,42 +14,45 @@ export class ImageUploaderComponent implements OnInit {
 
   @ViewChild('imageUploader', { static: true }) imageUploader: ElementRef;
 
-  constructor(public appModalService: AppModalService) { }
+  constructor(public appModalService: AppModalService, private imageCompress: NgxImageCompressService) { }
 
   ngOnInit() {
   }
 
   public uploadImage(event: any) {
-    console.log('EVENT: ', event);
-
-    // console.log('SECTION: ', this.section);
-
-    // if (!this.section?.sectionId) {
-    //   this.appModalService.ShowConfirmationModal(ModalTypes.InformationModal, 'Error uploading image', 'No section ID is present in request. Please contact administrator', null);
-    // }
-
-    // const file:File = event.target.files[0];
-    // console.log('FILE ARRAY: ', file);
-    // if (file) {
-    //   console.log('FILES: ', file);
-    // }
-
     let status = false;
     const file = event.target.files[0];
     status = event.target.files.length > 0 ? true : false;
 
-    console.log("FILE: ", file);
     if (status == true) {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
-        const imageSrc = reader.result;
-        // console.log('GOING TO EMIT: ', imageSrc);
+        const imageSrc = reader.result || '';
         const dataToEmit = new UploadImageRequest.RootObject();
-        dataToEmit.imageData = imageSrc;
         dataToEmit.imageName = file.name;
 
-        this.imageUploadedEmit.emit(dataToEmit);
+        if (file.size > 500000) {
+          const imageToCompress = {
+            image: imageSrc as string,
+            orientation: 1,
+            fileName: file.name
+          }
+          this.imageCompress
+            .getImageWithMaxSizeAndMetas(imageToCompress, 0.5) // this function can provide debug information using (MAX_MEGABYTE,true) parameters
+            .then(
+              (result) => {
+                dataToEmit.imageData = result.image;
+                this.imageUploadedEmit.emit(dataToEmit);
+              },
+              (result: string) => {
+                this.appModalService.ShowConfirmationModal(ModalTypes.InformationModal, 'Upload image', 'The file size was too big. Please try uploading a smaller file.', null);
+              }
+            );
+        } else {
+          dataToEmit.imageData = imageSrc;
+          this.imageUploadedEmit.emit(dataToEmit);
+        }
       }
     } else {
       this.appModalService.ShowConfirmationModal(ModalTypes.InformationModal, 'Error uploading image', 'No file was selected.', null);
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { FileUploaderComponent } from './components/file-uploader/file-uploader.
 import { ImageUploaderComponent } from './components/image-uploader/image-uploader.component';
 import { OnEnterDirective } from 'src/app/directives/on-enter/on-enter.directive';
 import { LoginComponent } from 'src/app/components/login/login.component';
+import { NgxImageCompressService } from 'ngx-image-compress';
 
 @NgModule({
   imports: [
@@ -29,6 +30,7 @@ import { LoginComponent } from 'src/app/components/login/login.component';
     MaterialModule,
     OnEnterDirective,
     LoginComponent
-  ]
+  ],
+  providers: [NgxImageCompressService]
 })
 export class SharedModule { }
